1bb61eafbdd1daafc0845f50c4b5486d90451480
SiddharthaSarma <sidduinevitable@gmail.com>
2017-07-24 13:49:09 +0530

    Added functionality to fetch the books from API.

--- public/reducers/BooksReducer.jsx
+++ public/reducers/BooksReducer.jsx
@@ -1,7 +1,10 @@
 export default function BooksReducer(state = [], action) {
   switch (action.type) {
     case "ADD_BOOK":
-      return [...state, action.data];
+      return state.concat(action.data);
+    case "FETCH_BOOKS":
+      console.log([...action.data])
+      return action.data;
     default:
       return state;
   }
