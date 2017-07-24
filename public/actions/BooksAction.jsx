6ccfe003fc8fd9676c3bf172146058086d9fea9c
SiddharthaSarma <sidduinevitable@gmail.com>
2017-07-24 13:51:05 +0530

    Added actions to fetch the books from API

--- public/actions/BooksAction.jsx
+++ public/actions/BooksAction.jsx
@@ -1,6 +1,21 @@
+import axios from "axios";
+
 export function addBook(data) {
-  return {
-    type: "ADD_BOOK",
-    data
+  const request = axios.post("http://localhost:3000/api/books", data);
+  return dispatch => {
+    request.then(response => {
+      dispatch({
+        type: "ADD_BOOK",
+        data
+      });
+    });
+  };
+}
+export function getBooks() {
+  const request = axios.get("http://localhost:3000/api/books");
+  return dispatch => {
+    request.then(({ data }) => {
+      dispatch({ type: "FETCH_BOOKS", data: data });
+    });
   };
 }
