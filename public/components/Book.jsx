c30a6fc33b66fe655ab2533d8805d4926304b80c
SiddharthaSarma <sidduinevitable@gmail.com>
2017-07-29 21:32:24 +0530

    Added delete icon to delete the book

--- public/components/Book.jsx
+++ public/components/Book.jsx
@@ -1,11 +1,28 @@
-import React from "react";
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import * as bookActions from "../actions/BooksAction";
+class Book extends Component {
+  constructor(props) {
+    super(props);
+    this.deleteBook = this.deleteBook.bind(this);
+  }
+  deleteBook() {
+    this.props.dispatch(bookActions.deleteBook(this.props._id));
+    console.log(this.props);
+  }
+  render() {
+    return (
+      <li className="list-group-item" key={this.props.key}>
+        {this.props.title}
+        <button
+          onClick={this.deleteBook}
+          className="btn btn-xs btn-danger pull-right"
+        >
+          <span className="glyphicon glyphicon-trash" />
+        </button>
+      </li>
+    );
+  }
+}
 
-const Book = props => {
-  return (
-    <li className="list-group-item" key={props.key}>
-      {props.title}
-    </li>
-  );
-};
-
-export default Book;
+export default connect()(Book);
